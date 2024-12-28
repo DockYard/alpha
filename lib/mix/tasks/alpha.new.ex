45f5048bc19f22b1f1d761fa5cac6c429dcde2cd
Brian Cardarella <bcardarella@gmail.com>
2024-12-27 20:18:32 -0500

    Checkin with spinner

---
+++ lib/mix/tasks/alpha.new.ex
@@ -0,0 +1,102 @@
+defmodule Mix.Tasks.Alpha.New do
+  @moduledoc """
+  Creates a new Alpha Stack project.
+
+  """
+  use Mix.Task
+
+  @spinners "⠁⠂⠄⡀⡁⡂⡄⡅⡇⡏⡗⡧⣇⣏⣗⣧⣯⣷⣿⢿⣻⢻⢽⣹⢹⢸⠸⢘⠘⠨⢈⠈⠐⠠⢀ " |> String.split("")
+
+  def run([name]) do
+    generate_project(name)
+  end
+
+  def run(_argv) do
+    raise "Missing app name"
+  end
+
+  defp generate_project(name) do
+    otp_app = Macro.underscore(name)
+
+    bindings = [
+      name: name,
+      otp_app: otp_app,
+      secret_key_base_dev: random_string(64),
+      secret_key_base_test: random_string(64),
+      signing_salt: random_string(8),
+      lv_signing_salt: random_string(8)
+    ]
+
+    from =
+      :code.priv_dir(:alpha)
+      |> List.to_string()
+      |> Path.join("templates/alpha.new")
+
+    to = otp_app
+
+    templates =
+      from
+      |> Path.join("/**/*")
+      |> Path.wildcard()
+      |> Enum.reject(&File.dir?/1)
+      |> Enum.map(fn(path) ->
+        path
+        |> String.split(from)
+        |> List.last()
+      end)
+
+    templates = [".gitignore", ".vscode/settings.json" | templates]
+
+    Enum.map(templates, &Path.dirname(&1))
+    |> Enum.uniq()
+    |> Enum.each(fn(path) ->
+      File.mkdir_p(Path.join(to, path))
+    end)
+
+    templates
+    |> Enum.each(fn(path) ->
+      file = EEx.eval_file(Path.join(from, path), assigns: bindings)
+      Mix.Generator.create_file(Path.join(to, path), file)
+    end)
+
+    File.cd!(otp_app, fn ->
+      Mix.shell().info([:green, "* running ", :reset, "mix setup"])
+      {:ok, task_pid} = Task.start(fn -> spinner({:wait, 0, 0}) end)
+      send(task_pid, :start)
+      Mix.Shell.cmd("mix setup", [], fn data ->
+        data
+        |> String.split(~r/(\r|\n)/, trim: true)
+        |> Enum.each(fn(line) ->
+          line = String.trim(line)
+          send(task_pid, {:length, String.length(line)})
+          IO.write("\r\e[2C\e[K#{String.trim(line)}")
+          Process.sleep(2)
+        end)
+      end)
+      IO.write("\r\e[K")
+      send(task_pid, :stop)
+    end)
+  end
+
+  defp spinner({stage, idx, length} = state) do
+    receive do
+      :start -> spinner({:start, idx, length})
+      {:length, new_length} -> spinner({stage, idx, new_length})
+      :stop -> :ok
+
+    after
+      0 ->
+        Process.sleep(50)
+        case state do
+          {:start, idx, length} ->
+            spinner = Enum.at(@spinners, rem(idx, length(@spinners)))
+            IO.write("\r#{spinner}\e[#{length + 3}G")
+            spinner({:start, idx + 1, length})
+          state -> spinner(state)
+        end
+    end
+  end
+
+  defp random_string(length),
+    do: :crypto.strong_rand_bytes(length) |> Base.encode64() |> binary_part(0, length)
+end
--- mix.exs
+++ mix.exs
@@ -1,14 +1,30 @@
 defmodule Alpha.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+  @source_url "https://github.com/DockYard/alpha"
+
   def project do
     [
       app: :alpha,
-      version: "0.l.1",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "The beginning",
+      package: package()
     ]
+  end
+
+  defp package do
+    %{
+      maintainers: ["Brian Cardarella"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url,
+        "Built by DockYard, Expert Elixir & Phoenix Consultants" => "https://dockyard.com/phoenix-consulting"
+      }
+    }
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -21,8 +37,7 @@ defmodule Alpha.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
